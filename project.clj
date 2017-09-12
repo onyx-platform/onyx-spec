0711649f92a93d9a4fbc2f184b8417c2067834ab
Michael Drogalis <mjd3089@rit.edu>
2017-09-12 14:38:18 -0700

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.onyxplatform/onyx-spec "0.11.0-SNAPSHOT"
+  :description "Clojure spec's for Onyx core."
+  :url "https://github.com/onyx-platform/onyx-spec"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
