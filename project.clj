d9a5ba67677867c83997a3e72c3a69b3962b9b4c
Michael Drogalis <mjd3089@rit.edu>
2017-09-12 14:41:30 -0700

    Ports specs.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.onyxplatform/onyx-spec "0.11.0-SNAPSHOT"
-  :description "Clojure spec's for Onyx core."
+  :description "Clojure specs for Onyx core."
   :url "https://github.com/onyx-platform/onyx-spec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]])
--- src/onyx_spec/core.clj
+++
@@ -1,6 +0,0 @@
-(ns onyx-spec.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
