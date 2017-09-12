2fcbcf437b2872baabc482d6448b75b700c51425
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-09-12 16:05:29 -0700

    Add to release process.

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "https://github.com/onyx-platform/onyx-spec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"snapshots" {:url "https://clojars.org/repo"
+                              :username :env
+                              :password :env
+                              :sign-releases false}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env
+                             :password :env
+                             :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.9.0-alpha20"]])
