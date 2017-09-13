42820d729d464d4f855653945c2fa2b29aaa4f39
OnyxBot <michael.drogalis@onyxplatform.org>
2017-09-13 23:16:15 +0000

    Release version 0.11.0.0-alpha5.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-spec "0.11.0.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-spec "0.11.0.0-alpha5"
   :description "Clojure specs for Onyx core."
   :url "https://github.com/onyx-platform/onyx-spec"
   :license {:name "Eclipse Public License"
