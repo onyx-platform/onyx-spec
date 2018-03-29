c247853b4cd727c0d07afb10c02c926372cb9957
Brian Marco <mail@brianmarco.com>
2018-03-29 10:23:58 -0400

    added clojure and cljs tests

--- project.clj
+++ project.clj
@@ -11,8 +11,20 @@
                              :username :env
                              :password :env
                              :sign-releases false}}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
+                 [org.clojure/clojurescript "1.10.238"]]
+  :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/js"]
+  :doo {:build "test"
+        :alias {:default [:chrome-headless]}}
+  :cljsbuild {:builds
+              [{:id "test"
+                :source-paths ["test"]
+                :compiler {:output-to "resources/public/js/compiled_test/onyx_sim_test.js"
+                           :output-dir "resources/public/js/compiled_test/out"
+                           :main onyx.spec.test-runner
+                           :optimizations :none}}]}
   :plugins [[codox "0.8.8"]
             [lein-set-version "0.4.1"]
             [lein-update-dependency "0.1.2"]
-            [lein-pprint "1.1.1"]])
+            [lein-pprint "1.1.1"]
+            [lein-doo "0.1.8"]])
