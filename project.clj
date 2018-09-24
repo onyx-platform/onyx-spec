fbb588b1323a59244d26cdc328566413d57984c4
OnyxBot <michael.drogalis@onyxplatform.org>
2018-09-24 05:01:43 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-spec "0.13.5.0"
+(defproject org.onyxplatform/onyx-spec "0.13.5.1-SNAPSHOT"
   :description "Clojure specs for Onyx core."
   :url "https://github.com/onyx-platform/onyx-spec"
   :license {:name "Eclipse Public License"
